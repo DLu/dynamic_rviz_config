30ee4373714700aaa0f0b8b9ce1c098df0b0e94c
David V. Lu!! <davidvlu@gmail.com>
2015-07-08 16:47:54 -0400

    Add Displays Object

--- src/dynamic_rviz_config/__init__.py
+++ src/dynamic_rviz_config/__init__.py
@@ -1,6 +1,9 @@
 import yaml
 import rospy
 import tempfile
+from dynamic_rviz_config.displays import Displays
+
+VIZ_MAN = 'Visualization Manager'
 
 class RVizConfig:
     def __init__(self, base_file=None):
@@ -8,45 +11,23 @@ class RVizConfig:
             self.data = yaml.load( base_file )
         else:
             self.data = {}
-            
-    def add_display(self, name, class_name, topic=None, color=None, fields={}):
-        print name, topic
-        d = {'Name': name, 'Class': class_name, 'Enabled': True}
-        if topic:
-            d['Topic'] = topic
-        if color:
-            d['Color'] = '%d; %d; %d'%color
-        d.update(fields)
-        self.data['Visualization Manager']['Displays'].append(d)
+
+    def get_visualization(self):
+        if VIZ_MAN not in self.data:
+            self.data[VIZ_MAN] = {}
+        return self.data[VIZ_MAN]            
+
+    def get_displays(self):
+        if 'Displays' not in self.get_visualization():
+            self.data[VIZ_MAN]['Displays'] = Displays()
+        return self.data[VIZ_MAN]['Displays']    
         
     def set_tool_topic(self, name, topic):
-        for m in self.data['Visualization Manager']['Tools']:
+        for m in self.data[VIZ_MAN]['Tools']:
             if m.get('Class', '')==name:
                 m['Topic'] = topic
                 return
-        
-    def add_model(self, parameter='robot_description'):
-        self.add_display('RobotModel', 'rviz/RobotModel', fields={'Robot Description': parameter})
-        
-    def add_map(self, topic='/map', name='Map'):
-        self.add_display(name, 'rviz/Map', topic)
-        
-    def add_laserscan(self, topic='/base_scan', color=(46, 255, 0)):
-        self.add_display(topic, 'rviz/LaserScan', topic, color, 
-            {'Size (m)': .1, 'Style': 'Spheres', 'Color Transformer': 'FlatColor'})
 
-    def add_pose_array(self, topic='/particlecloud'):
-        self.add_display('AMCL Cloud', 'rviz/PoseArray', topic)
-        
-    def add_footprint(self, topic, color=(0,170,255)):
-        self.add_display('Robot Footprint', 'rviz/Polygon', topic, color)
-        
-    def add_path(self, topic, name, color=None):
-        self.add_display(name, 'rviz/Path', topic, color)
-
-    def add_pose(self, topic):
-        self.add_display('Current Goal', 'rviz/Pose', topic)
-        
     def set_goal(self, topic):
         self.set_tool_topic('rviz/SetGoal', topic)
         
---
+++ src/dynamic_rviz_config/displays.py
@@ -0,0 +1,43 @@
+
+class Displays(list):
+    def __init__(self):
+        None
+        
+    def add_display(self, name, class_name, topic=None, color=None, fields={}):
+        print name, topic
+        d = {'Name': name, 'Class': class_name, 'Enabled': True}
+        if topic:
+            d['Topic'] = topic
+        if color:
+            d['Color'] = '%d; %d; %d'%color
+        d.update(fields)
+        self.append(d)
+        
+    def add_model(self, parameter='robot_description'):
+        self.add_display('RobotModel', 'rviz/RobotModel', fields={'Robot Description': parameter})
+        
+    def add_map(self, topic='/map', name='Map'):
+        self.add_display(name, 'rviz/Map', topic)
+        
+    def add_laserscan(self, topic='/base_scan', color=(46, 255, 0)):
+        self.add_display(topic, 'rviz/LaserScan', topic, color, 
+            {'Size (m)': .1, 'Style': 'Spheres', 'Color Transformer': 'FlatColor'})
+
+    def add_pose_array(self, topic='/particlecloud'):
+        self.add_display('AMCL Cloud', 'rviz/PoseArray', topic)
+        
+    def add_footprint(self, topic, color=(0,170,255)):
+        self.add_display('Robot Footprint', 'rviz/Polygon', topic, color)
+        
+    def add_path(self, topic, name, color=None):
+        self.add_display(name, 'rviz/Path', topic, color)
+
+    def add_pose(self, topic):
+        self.add_display('Current Goal', 'rviz/Pose', topic)
+
+import yaml
+
+def display_representer(dumper, data):
+    return dumper.represent_sequence(u'tag:yaml.org,2002:seq', list(data))
+
+yaml.add_representer(Displays, display_representer)
